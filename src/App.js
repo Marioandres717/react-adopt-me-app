4818ca1091d528d57f403072a17524b1b1c4574e
Mario Andres <Marioarendon2@gmail.com>
2019-03-12 13:15:25 -0400

    Eslint configuration for react

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Jake',
-      animal: 'Cat',
-      breed: 'Boss'
-    }),
-    React.createElement(Pet, {
-      name: 'Tata',
-      animal: 'Dog',
-      breed: 'Boxer'
-    }),
-    React.createElement(Pet, {
-      name: 'Yaya',
-      animal: 'Dog',
-      breed: 'French Puddle'
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name="Jake" animal="Cat" breed="Boss" />
+      <Pet name="Tata" animal="Dog" breed="Boxer" />
+      <Pet name="Yaya" animal="Dog" breed="French Puddle" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
