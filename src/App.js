26b60c86e2d07ba38338cbb62ea0637febc922c1
Mario Andres <Marioarendon2@gmail.com>
2019-03-12 11:51:50 -0400

    Prettier setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Jake',
+      animal: 'Cat',
+      breed: 'Boss'
+    }),
+    React.createElement(Pet, {
+      name: 'Tata',
+      animal: 'Dog',
+      breed: 'Boxer'
+    }),
+    React.createElement(Pet, {
+      name: 'Yaya',
+      animal: 'Dog',
+      breed: 'French Puddle'
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
